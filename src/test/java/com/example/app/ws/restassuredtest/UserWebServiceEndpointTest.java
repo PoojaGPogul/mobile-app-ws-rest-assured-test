232d765173b2752a2421f49322524fd88207b271
PoojaGPogul <poojapogul@gmail.com>
2021-04-22 22:00:58 +0530

    210. Update User Details API Call: Validating HTTP Response

--- src/test/java/com/example/app/ws/restassuredtest/UserWebServiceEndpointTest.java
+++ src/test/java/com/example/app/ws/restassuredtest/UserWebServiceEndpointTest.java
@@ -24,6 +24,7 @@ class UserWebServiceEndpointTest {
 	private final String JSON = "application/json";
 	private static String userId;
 	private static String authorizationHeader;
+	private static List<Map<String, String>> addresses;
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -59,32 +60,62 @@ class UserWebServiceEndpointTest {
 	 */
 	@Test
 	final void b() {
-		Response response = given()
-				.pathParam("id", userId)
+		Response response = given().pathParam("id", userId)
 				.header("Authorization", authorizationHeader).accept(JSON)
 				.when().get(CONTEXT_PATH + "/users/{id}")
-				.then().statusCode(200).contentType(JSON)
-				.extract().response();
-		
+				.then().statusCode(200).contentType(JSON).extract().response();
+
 		String userPublicId = response.jsonPath().getString("userId");
 		String email = response.jsonPath().getString("email");
 		String firstName = response.jsonPath().getString("firstName");
 		String lastName = response.jsonPath().getString("lastName");
-		
+
 		assertNotNull(userPublicId);
 		assertNotNull(email);
 		assertNotNull(firstName);
 		assertNotNull(lastName);
 		assertEquals(EMAIL_ADDRESS, email);
-		
-		List<Map<String, String>> addresses = response.jsonPath().getList("addresses");
+
+		addresses = response.jsonPath().getList("addresses");
 		assertNotNull(addresses);
 		assertTrue(addresses.size() == 1);
-		
+
 		String addressId = addresses.get(0).get("addressId");
 		assertNotNull(addressId);
 		assertTrue(addressId.length() == 30);
+
+	}
+
+	/**
+	 * testUpdateUserDetails()
+	 */
+	@Test
+	final void c() {
+		Map<String, Object> userDetails = new HashMap<String, Object>();
+		userDetails.put("firstName", "ABC");
+		userDetails.put("lastName", "XYZ");		
 		
+		Response response = given().contentType(JSON).accept(JSON)
+				.header("Authorization", authorizationHeader)
+				.pathParam("id", userId)
+				.body(userDetails)
+				.when().put(CONTEXT_PATH + "/users/{id}")
+				.then().statusCode(200)
+				.contentType(JSON).extract().response();
+		
+		
+		String firstName = response.jsonPath().getString("firstName");
+		String lastName = response.jsonPath().getString("lastName");
+		
+		assertNotNull(firstName);
+		assertNotNull(lastName);
+		assertEquals("ABC", firstName);
+		assertEquals("XYZ", lastName);
+		
+		List<Map<String, String>> storedAddresses = response.jsonPath().getList("addresses");
+		assertNotNull(storedAddresses);
+		assertEquals(addresses.size(), storedAddresses.size());
+		assertEquals(addresses.get(0).get("streetName"), storedAddresses.get(0).get("streetName"));
 		
 	}
 
